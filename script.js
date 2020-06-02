c9a9b1e5cf15475f397f4adea8527e112fbdb934
A <jesus.e.estrada@hotmail.com>
2020-06-02 15:03:58 -0500

    added jquery

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+'use strict';
+
+
 function pressSubmit() {
-    $('.button').click(event =>{ 
-        event.preventDefault();
-        
-    })
+  $('#number-chooser').submit(event => {
+    event.preventDefault();
+
+    $('.js-result').empty();
+
+    let number = parseInt($(event.currentTarget).find('input[name="number-choice"]').val());
+
+    let results = [];
+
+    for (let i = 1; i <= number; i++) {
+      if (i % 3 === 0 && i % 5 === 0) {
+        results.push('fizzbuzz');
+      } else if (i % 3 === 0) {
+        results.push('fizz');
+      } else if (i % 5 === 0) {
+        results.push('buzz');
+      } else {
+        results.push(i);
+      }
+    }
+
+    $('.js-results').append(results);
+
+  });
 }
+
+$(pressSubmit());
